61dca5f64ee5b04404247b01881e0a168274e127
KleiKodesh <211884665+KleiKodesh@users.noreply.github.com>
2026-08-27 22:59:47 +0300

    document locator service bug fix

--- Build/Installer/Helpers/AddinInstaller.cs
+++ Build/Installer/Helpers/AddinInstaller.cs
@@ -20,7 +20,7 @@ namespace KleiKodeshVstoInstallerWpf.Helpers
     {
         public const string AppName         = "KleiKodesh";
         public const string AppDisplayName  = "כלי קודש";
-        public const string Version         = "v10.0.0";
+        public const string Version         = "v9.1.0";
         public const string InstallFolderName = "KleiKodesh";
         public const string VstoFileName    = "KleiKodesh.vsto";
 
--- KitveiHakodesh/CSharpBackend/KitveiHakodeshDemoApp/Properties/AssemblyInfo.cs
+++ KitveiHakodesh/CSharpBackend/KitveiHakodeshDemoApp/Properties/AssemblyInfo.cs
@@ -29,5 +29,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("10.0.0.0")]
-[assembly: AssemblyFileVersion("10.0.0.0")]
+[assembly: AssemblyVersion("9.1.0.0")]
+[assembly: AssemblyFileVersion("9.1.0.0")]
